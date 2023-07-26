553c675ff0728b55cbb51d1a1cf83aad0dbf8892
Eric Onasis <ericonasis@gmail.com>
2023-07-26 21:24:26 +0300

    Fix Environmental Variables

--- shell.c
+++ shell.c
@@ -8,45 +8,57 @@
  * Return: _exit = 0.
  */
 
-int main(int argc, char **argv, char **env)
+/**
+ * main - Main arguments function
+ * @argc: Count of arguments
+ * @argv: Arguments
+ * @envp: Environment
+ * Return: _exit = 0
+ */
+
+
+int main(int argc, char **argv, char **envp)
 {
-	char *getcommand = NULL, **user_command = NULL;
-	int pathValue = 0, _exit = 0, n = 0;
+	char *userInput = NULL, **userTokens = NULL;
+	int pathValue = 0, exitCode = 0, n = 0;
 	(void)argc;
 
 	while (1)
 	{
-		getcommand = get_user_input();
-		if (getcommand)
+		userInput = get_user_input();
+		if (userInput)
 		{
 			pathValue++;
-			user_command = tokenize_input(getcommand);
-			if (!user_command)
+			userTokens = tokenize_input(userInput);
+			if (!userTokens)
 			{
-				free(getcommand);
+				free(userInput);
 				continue;
 			}
-			if ((!_strcmp(user_command[0], "exit")) && user_command[1] == NULL)
-				exit_command(user_command, getcommand, _exit);
-			if (!_strcmp(user_command[0], "env"))
-				print_env(env);
+
+			if ((!_strcmp(userTokens[0], "exit")) && userTokens[1] == NULL)
+				exit_command(userTokens, userInput, exitCode);
+			if (!_strcmp(userTokens[0], "env"))
+				print_env(envp);
 			else
 			{
-				n = find_path(user_command, env);
-				_exit = fork_and_execute(user_command,
-				argv, env, getcommand, pathValue, n);
+				n = find_path(userTokens, envp);
+				exitCode = fork_and_execute(userTokens, argv, envp, userInput, pathValue, n);
 				if (n == 0)
-					free(user_command[0]);
+					free(userTokens[0]);
 			}
-			free(user_command);
+
+			free(userTokens);
 		}
 		else
 		{
 			if (isatty(STDIN_FILENO))
 				write(STDOUT_FILENO, "\n", 1);
-			exit(_exit);
+			exit(exitCode);
 		}
-		free(getcommand);
+
+		free(userInput);
 	}
-	return (_exit);
+
+	return (exitCode);
 }
